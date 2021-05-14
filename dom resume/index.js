c32f7014221109e4f8d64d21c12a909206fdf360
noussairox <abellouch34@gmail.com>
2021-05-14 02:17:25 +0000

    new change

---
+++ dom resume/index.js
@@ -0,0 +1,9 @@
+// ADDING ELEMENT
+const body = document.body
+body.append("hello"," Hamid")
+// Creating Element
+const div = document.createElement("div")
+div.innerText="hello 3alam"
+body.append(div)
+//MODIFY ELEMENT HTML
+div.innerHTML = "<strong>3alam</strong>"
